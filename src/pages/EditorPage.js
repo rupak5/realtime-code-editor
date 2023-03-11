380b3d2870bdb1d17843bc452a93351195ee626d
rupak5 <65965671+rupak5@users.noreply.github.com>
2023-03-12 00:42:11 +0530

    Added Code for Code Compile and Chat

--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -10,6 +10,7 @@ import {
   useParams,
 } from "react-router-dom";
 import { toast } from "react-hot-toast";
+import axios from "axios";
 
 const EditorPage = () => {
   const socketRef = useRef(null);
@@ -60,6 +61,15 @@ const EditorPage = () => {
           return prev.filter((client) => client.socketId !== socketId);
         });
       });
+
+      //Listening for message
+      socketRef.current.on(ACTIONS.SEND_MESSAGE, ({ message }) => {
+        const chatWindow = document.getElementById("chatWindow");
+        var currText = chatWindow.value;
+        currText += message;
+        chatWindow.value = currText;
+        chatWindow.scrollTop = chatWindow.scrollHeight;
+      });
     };
     init();
     return () => {
@@ -87,6 +97,98 @@ const EditorPage = () => {
     return <Navigate to="/" />;
   }
 
+  const inputClicked = () => {
+    const inputArea = document.getElementById("input");
+    inputArea.placeholder = "Enter your input here";
+    inputArea.value = "";
+    inputArea.disabled = false;
+    const inputLabel = document.getElementById("inputLabel");
+    const outputLabel = document.getElementById("outputLabel");
+    inputLabel.classList.remove("notClickedLabel");
+    inputLabel.classList.add("clickedLabel");
+    outputLabel.classList.remove("clickedLabel");
+    outputLabel.classList.add("notClickedLabel");
+  };
+
+  const outputClicked = () => {
+    const inputArea = document.getElementById("input");
+    inputArea.placeholder =
+      "You output will apear here, Click 'Run code' to see it";
+    inputArea.value = "";
+    inputArea.disabled = true;
+    const inputLabel = document.getElementById("inputLabel");
+    const outputLabel = document.getElementById("outputLabel");
+    inputLabel.classList.remove("clickedLabel");
+    inputLabel.classList.add("notClickedLabel");
+    outputLabel.classList.remove("notClickedLabel");
+    outputLabel.classList.add("clickedLabel");
+  };
+
+  const runCode = () => {
+    const lang = document.getElementById("languageOptions").value;
+    const input = document.getElementById("input").value;
+    const code = codeRef.current;
+
+    toast.loading("Running Code....");
+
+    const encodedParams = new URLSearchParams();
+    encodedParams.append("LanguageChoice", lang);
+    encodedParams.append("Program", code);
+    encodedParams.append("Input", input);
+
+    const options = {
+      method: "POST",
+      url: "https://code-compiler.p.rapidapi.com/v2",
+      headers: {
+        "content-type": "application/x-www-form-urlencoded",
+        "X-RapidAPI-Key": process.env.REACT_APP_API_KEY,
+        "X-RapidAPI-Host": "code-compiler.p.rapidapi.com",
+      },
+      data: encodedParams,
+    };
+
+    console.log(options);
+
+    axios
+      .request(options)
+      .then(function (response) {
+        let message = response.data.Result;
+        if (message === null) {
+          message = response.data.Errors;
+        }
+        outputClicked();
+        document.getElementById("input").value = message;
+        toast.dismiss();
+        toast.success("Code compilation complete");
+      })
+      .catch(function (error) {
+        toast.dismiss();
+        toast.error("Code compilation unsuccessful");
+        document.getElementById("input").value =
+          "Something went wrong, Please check your code and input.";
+      });
+  };
+
+  const sendMessage = () => {
+    if (document.getElementById("inputBox").value === "") return;
+    var message = `${location.state.username}: ${
+      document.getElementById("inputBox").value
+    }\n`;
+    const chatWindow = document.getElementById("chatWindow");
+    var currText = chatWindow.value;
+    currText += message;
+    chatWindow.value = currText;
+    chatWindow.scrollTop = chatWindow.scrollHeight;
+    document.getElementById("inputBox").value = "";
+    socketRef.current.emit(ACTIONS.SEND_MESSAGE, { roomId, message });
+  };
+
+  const handleInputEnter = (key) => {
+    if (key.code === "Enter") {
+      sendMessage();
+    }
+  };
+
   return (
     <div className="mainWrap">
       <div className="aside">
@@ -101,6 +203,30 @@ const EditorPage = () => {
             ))}
           </div>
         </div>
+        <label>
+          Select Language:
+          <select id="languageOptions" className="seLang" defaultValue="17">
+            <option value="1">C#</option>
+            <option value="4">Java</option>
+            <option value="5">Python</option>
+            <option value="6">C (gcc)</option>
+            <option value="7">C++ (gcc)</option>
+            <option value="8">PHP</option>
+            <option value="11">Haskell</option>
+            <option value="12">Ruby</option>
+            <option value="13">Perl</option>
+            <option value="17">Javascript</option>
+            <option value="20">Golang</option>
+            <option value="21">Scala</option>
+            <option value="37">Swift</option>
+            <option value="38">Bash</option>
+            <option value="43">Kotlin</option>
+            <option value="60">TypeScript</option>
+          </select>
+        </label>
+        <button className="btn runBtn" onClick={runCode}>
+          Run Code
+        </button>
         <button className="btn copyBtn" onClick={copyRoomId}>
           Copy ROOM ID
         </button>
@@ -108,6 +234,7 @@ const EditorPage = () => {
           Leave
         </button>
       </div>
+
       <div className="editorWrap">
         <Editor
           socketRef={socketRef}
@@ -116,6 +243,48 @@ const EditorPage = () => {
             codeRef.current = code;
           }}
         />
+        <div className="IO-container">
+          <label
+            id="inputLabel"
+            className="clickedLabel"
+            onClick={inputClicked}
+          >
+            Input
+          </label>
+          <label
+            id="outputLabel"
+            className="notClickedLabel"
+            onClick={outputClicked}
+          >
+            Output
+          </label>
+        </div>
+        <textarea
+          id="input"
+          className="inputArea textarea-style"
+          placeholder="Enter your input here"
+        ></textarea>
+      </div>
+
+      <div className="chatWrap">
+        <textarea
+          id="chatWindow"
+          className="chatArea textarea-style"
+          placeholder="Chat messages will appear here"
+          disabled
+        ></textarea>
+        <div className="sendChatWrap">
+          <input
+            id="inputBox"
+            type="text"
+            placeholder="Type your message here"
+            className="inputField"
+            onKeyUp={handleInputEnter}
+          ></input>
+          <button className="btn sendBtn" onClick={sendMessage}>
+            Send
+          </button>
+        </div>
       </div>
     </div>
   );
